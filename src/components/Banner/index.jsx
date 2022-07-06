9596b316797549d6ee5370b137fb41b54992ff64
Caiquegam <caiquegamadeoliveira@gmail.com>
2022-07-06 08:48:58 -0300

    readme

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 
 import "./style.css"
-
+import bannerImage from "../../assets/Banner.jpg"
 function Banner() {
     return(
-        <div className="bannerContainer"></div>
+        <div className="bannerContainer">
+            <img src={bannerImage}/>
+        </div>
     )
 }
 
